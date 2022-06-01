dbda1fe4573ca77a79cb47b3bfb8adc0e48fe827
Stewart Chan <stew.chan@gmail.com>
2022-06-01 13:14:59 -0400

    added title

--- TitleWorld.java
+++ TitleWorld.java
@@ -17,5 +17,7 @@ public class TitleWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        Label label = new Label("My Game", 100);
+        addObject(label, 300, 200);
     }
 }
